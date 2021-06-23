5bff9d6c8a1478e4414c0fa0cb9d322716b5db0a
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-06-22 19:13:19 -0700

    Added the normal sampler, resizing for the orthogonal space.

--- include/irlba/utils.hpp
+++ include/irlba/utils.hpp
@@ -2,6 +2,7 @@
 #define IRLBA_UTILS_HPP
 
 #include "Eigen/Dense"
+#include <random>
 
 namespace irlba {
 
@@ -13,8 +14,13 @@ public:
     /**
      * @param max Expected maximum number of columns in `mat`, to be used to construct the workspace.
      */
-    OrthogonalizeVector(int max=10) : tmp(max) {}
+    OrthogonalizeVector() : {}
 
+    OrthogonalizeVector& set_size(int max) {
+        tmp.resize(max);
+        return *this;
+    }
+public:
     /**
      * @param mat A matrix where the left-most `ncols` columns are orthonormal vectors.
      * @param vec The vector of interest, of length equal to the number of rows in `mat`.
@@ -30,6 +36,23 @@ public:
     }
 private:
     Eigen::VectorXd tmp;
+};
+
+class NormalSampler {
+public:
+    NormalSampler(size_t s) : generator(s) {}
+    double operator()() { 
+        return distr(generator);
+    }
+private:
+    std::mt19937_64 generator;
+
+#ifdef IRLBA_RANDOM_REPRODUCIBLE
+    // Probably should use Boost's method here, to ensure we 
+    // get the same implementation across compilers.
+#else 
+    std::normal_distribution<double> distr;
+#endif
 };
 
 }
