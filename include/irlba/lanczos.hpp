0f544432f685c70355cf27db01743aa9c181ec63
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-06-22 19:10:53 -0700

    Added the pre-loop sections of the Lanczos process.

--- include/irlba/lanczos.hpp
+++ include/irlba/lanczos.hpp
@@ -2,21 +2,77 @@
 #define IRLBA_LANCZOS_HPP
 
 #include "Eigen/Dense"
+#include "utils.hpp"
+#include <cmath>
+#include <limits>
 
 namespace irlba {
     
 class LanczosProcess {
 public:
-    template<class M>
-    void operator()(const M& mat, Eigen::MatrixXd& W, Eigen::MatrixXd& V, double S) {
-        for (int j = 0; j < work; ++j) {
+    LanczosProcess() {} 
+public:
+    static constexpr double default_eps = std::pow(std::numeric_limits<double>::epsilon, 0.8); // inherited from irlba.
+
+    LanczosProcess& set_eps(double e = default_eps) {
+        eps = e;
+        return *this;
+    }
+
+public:
+    template<class M, class CENTER, class SCALE, class NORMSAMP>
+    void operator()(const M& mat, Eigen::MatrixXd& W, Eigen::MatrixXd& V, const CENTER& center, const SCALE& scale, NORMSAMP& norm, int work, int start, bool first) {
+        constexpr bool do_center = !std::is_same<CENTER, bool>::value;
+        constexpr bool do_scale = !std::is_same<SCALE, bool>::value;
+
+        orthog.set_size(work);
+        F.resize(mat.cols());
+        W_next.resize(mat.rows());
+
+        // Doing some preparatory work.
+        if (start == 0 && first) {
+            double d = V.col(0).norm();
+            if (d < eps) {
+                throw -1; // TODO: try refilling with another random normal?
+            }
+            V.col(0) /= d;
+        }
+
+        F.noalias() = V.col(start);
+
+        if constexpr(do_scale) {
+            F /= scale;
+        }
+
+        W_next.noalias() = mat * F;
+
+        if constexpr(do_center) {
+            double beta = F.dot(center);
+            for (auto& w : W_next) {
+                w -= beta;
+            }
+        }
+
+        if (start && !first) {
+            orthog(W, W_next, start);
+        }
+
+        double S = W_next.norm();
+        if (S < eps && start == 0) {
+            throw -4; // TODO: try refilling with another random normal? And what happens if S < eps and start > 0; div by zero?
+        }
+        W_next /= S;
+        W.col(start) = W_next;
+
+        // The Lanczos iterations themselves.
+        for (int j = start; j < work; ++j) {
             F.noalias() = mat.adjoint() * W.col(j);
 
             // Centering and scaling, if requested.
-            if (center.size())  {
+            if constexpr(do_center) {
                 F.noalias() -= center;
             }
-            if (scale.size()) {
+            if constexpr(do_scale) {
                 F.noalias() /= scale;
             }
 
@@ -47,15 +103,15 @@ public:
                 auto& x = F;
 
                 // Applying the scaling.
-                if (scale.size()) {
+                if constexpr(do_scale) {
                     x /= scale;
                 }
 
-                W_next.noalias() = mat * F;
+                W_next.noalias() = mat * x;
 
                 // Applying the centering.
-                if (center.size()) {
-                    double beta = F.dot(center);
+                if constexpr(do_center) {
+                    double beta = x.dot(center);
                     for (auto& x : W_next) {
                         x -= beta;
                     }
@@ -71,7 +127,8 @@ public:
                 if (S < eps) {
                     for (auto& w : W_next) { w = norm(); }
                     orthog(W, W_next, j + 1);
-                    W_next /= F.norm();
+                    S = W_next.norm();
+                    W_next /= S;
                     S = 0;
                 } else {
                     W_next /= S;
@@ -82,20 +139,17 @@ public:
                 B(j, j) = S;
             }
         }
+
+        return 0;
     }
 
 private:
-    int work;
-    double eps;
-
-    Eigen::VectorXd F, scale, center;
-    Eigen::VectorXd W_next;
-
     OrthogonalizeVector orthog;
+    double eps = default_eps;
 
-    NormalSampler norm;
+    Eigen::VectorXd F; 
+    Eigen::VectorXd W_next;
 };
-
 
 }
 
