3aa93f0de70a22430e1c6cd079bca22455cc94d2
LTLA <LTLA@users.noreply.github.com>
2021-10-19 06:45:22 +0000

    Deploying to gh-pages from @ LTLA/CppIrlba@4608ce9030d87ee043afab43bf9146c76671ad4b 🚀

--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['adjoint_5fmultiply_0',['adjoint_multiply',['../structirlba_1_1Centered.html#a9296ea17bb1ff575731822c0b936bb3b',1,'irlba::Centered::adjoint_multiply()'],['../structirlba_1_1Scaled.html#ae475d9ac3e54e70c624d3c156ce14611',1,'irlba::Scaled::adjoint_multiply()']]]
+  ['adjoint_5fmultiply_0',['adjoint_multiply',['../structirlba_1_1Centered.html#ab3c2e5047238c2cd88f16dc96eff9c45',1,'irlba::Centered::adjoint_multiply()'],['../structirlba_1_1Scaled.html#aaa3ce90d6c94039b0831127b1e77b534',1,'irlba::Scaled::adjoint_multiply()']]]
 ];
--- docs/search/all_5.js
+++ docs/search/all_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['multiply_0',['multiply',['../structirlba_1_1Centered.html#a1f95cde076fde69b9dac77238fe77c5a',1,'irlba::Centered::multiply()'],['../structirlba_1_1Scaled.html#a6cd98a57b752d17489aa4ebd3008357d',1,'irlba::Scaled::multiply()']]]
+  ['multiply_0',['multiply',['../structirlba_1_1Centered.html#a67193ab497a4a860153bee60d0b741e2',1,'irlba::Centered::multiply()'],['../structirlba_1_1Scaled.html#ad9e3be8e5e9638e7c65425fe6f11894c',1,'irlba::Scaled::multiply()']]]
 ];
