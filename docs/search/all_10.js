86ab99fff1f8390613221c4a87eb7437c3b1b244
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-03-14 21:03:24 +0000

    Deploying to gh-pages from @ LTLA/CppIrlba@04273aca0c4ca3838fa7429a32607eff14a2df77 🚀

--- docs/search/all_10.js
+++ docs/search/all_10.js
@@ -2,9 +2,11 @@ var searchData=
 [
   ['workspace_0',['Workspace',['../structirlba_1_1Scaled_1_1Workspace.html',1,'irlba::Scaled&lt; Matrix &gt;::Workspace'],['../classirlba_1_1ParallelSparseMatrix.html#ace0a9677ba2415af992719ef0ff88ef5',1,'irlba::ParallelSparseMatrix::Workspace()'],['../structirlba_1_1Centered.html#aa86585b7bae6f19ae08d9a4baebc84fa',1,'irlba::Centered::Workspace()']]],
   ['workspace_1',['workspace',['../classirlba_1_1ParallelSparseMatrix.html#aede6b0baadeb52d351c25ab9f24da3dd',1,'irlba::ParallelSparseMatrix::workspace()'],['../structirlba_1_1Centered.html#a55b851d87fb995a2a04d3a07e69121ee',1,'irlba::Centered::workspace()'],['../structirlba_1_1Scaled.html#a0ad062d5091c3d8a9537e1f4debe669a',1,'irlba::Scaled::workspace()']]],
-  ['wrapped_5fadjoint_5fworkspace_2',['wrapped_adjoint_workspace',['../wrappers_8hpp.html#a2afbaa693ad02acc03ee74a3ea084630',1,'irlba']]],
-  ['wrapped_5fworkspace_3',['wrapped_workspace',['../wrappers_8hpp.html#afb93f4e9565318e79782c847fbebe16b',1,'irlba']]],
-  ['wrappedadjointworkspace_4',['WrappedAdjointWorkspace',['../wrappers_8hpp.html#abf539bafcd5bf49b8106fa00b7cf61a3',1,'irlba']]],
-  ['wrappedworkspace_5',['WrappedWorkspace',['../wrappers_8hpp.html#a06d0182fc36d37483e1c4731b966cf16',1,'irlba']]],
-  ['wrappers_2ehpp_6',['wrappers.hpp',['../wrappers_8hpp.html',1,'']]]
+  ['wrapped_5fadjoint_5fmultiply_2',['wrapped_adjoint_multiply',['../wrappers_8hpp.html#adb3751dd2a2de0754fe92d25a658b45c',1,'irlba']]],
+  ['wrapped_5fadjoint_5fworkspace_3',['wrapped_adjoint_workspace',['../wrappers_8hpp.html#a2afbaa693ad02acc03ee74a3ea084630',1,'irlba']]],
+  ['wrapped_5fmultiply_4',['wrapped_multiply',['../wrappers_8hpp.html#a944729e08e958d309cce1d66761e88c6',1,'irlba']]],
+  ['wrapped_5fworkspace_5',['wrapped_workspace',['../wrappers_8hpp.html#afb93f4e9565318e79782c847fbebe16b',1,'irlba']]],
+  ['wrappedadjointworkspace_6',['WrappedAdjointWorkspace',['../wrappers_8hpp.html#abf539bafcd5bf49b8106fa00b7cf61a3',1,'irlba']]],
+  ['wrappedworkspace_7',['WrappedWorkspace',['../wrappers_8hpp.html#a06d0182fc36d37483e1c4731b966cf16',1,'irlba']]],
+  ['wrappers_2ehpp_8',['wrappers.hpp',['../wrappers_8hpp.html',1,'']]]
 ];
--- docs/search/functions_c.js
+++ docs/search/functions_c.js
@@ -1,6 +1,8 @@
 var searchData=
 [
   ['workspace_0',['workspace',['../classirlba_1_1ParallelSparseMatrix.html#aede6b0baadeb52d351c25ab9f24da3dd',1,'irlba::ParallelSparseMatrix::workspace()'],['../structirlba_1_1Centered.html#a55b851d87fb995a2a04d3a07e69121ee',1,'irlba::Centered::workspace()'],['../structirlba_1_1Scaled.html#a0ad062d5091c3d8a9537e1f4debe669a',1,'irlba::Scaled::workspace()']]],
-  ['wrapped_5fadjoint_5fworkspace_1',['wrapped_adjoint_workspace',['../wrappers_8hpp.html#a2afbaa693ad02acc03ee74a3ea084630',1,'irlba']]],
-  ['wrapped_5fworkspace_2',['wrapped_workspace',['../wrappers_8hpp.html#afb93f4e9565318e79782c847fbebe16b',1,'irlba']]]
+  ['wrapped_5fadjoint_5fmultiply_1',['wrapped_adjoint_multiply',['../wrappers_8hpp.html#adb3751dd2a2de0754fe92d25a658b45c',1,'irlba']]],
+  ['wrapped_5fadjoint_5fworkspace_2',['wrapped_adjoint_workspace',['../wrappers_8hpp.html#a2afbaa693ad02acc03ee74a3ea084630',1,'irlba']]],
+  ['wrapped_5fmultiply_3',['wrapped_multiply',['../wrappers_8hpp.html#a944729e08e958d309cce1d66761e88c6',1,'irlba']]],
+  ['wrapped_5fworkspace_4',['wrapped_workspace',['../wrappers_8hpp.html#afb93f4e9565318e79782c847fbebe16b',1,'irlba']]]
 ];
