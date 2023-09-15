ef34465d790467a7034a07c1a5a78132552812e8
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-09-15 22:54:31 +0000

    Deploying to gh-pages from @ LTLA/CppIrlba@e7f4114ef5a42e9e2d3f5eee61d1af6955e3c1c7 🚀

--- docs/search/all_3.js
+++ docs/search/all_3.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['eigenthreadscope_0',['eigenthreadscope',['../classirlba_1_1EigenThreadScope.html',1,'irlba::EigenThreadScope'],['../classirlba_1_1EigenThreadScope.html#a8fdbc24d92c1e6610c8d5785bc575804',1,'irlba::EigenThreadScope::EigenThreadScope()']]],
-  ['extra_5fwork_1',['extra_work',['../structirlba_1_1Irlba_1_1Defaults.html#a5bcbb0f825ebb62a2626de2399cb6e6a',1,'irlba::Irlba::Defaults']]]
+  ['exact_5ffor_5flarge_5fnumber_1',['exact_for_large_number',['../structirlba_1_1Irlba_1_1Defaults.html#a922562b3c2203e40daf4769b58aacb79',1,'irlba::Irlba::Defaults']]],
+  ['exact_5ffor_5fsmall_5fmatrix_2',['exact_for_small_matrix',['../structirlba_1_1Irlba_1_1Defaults.html#a0aeacc87240ac67d1ec26daebacbc84d',1,'irlba::Irlba::Defaults']]],
+  ['extra_5fwork_3',['extra_work',['../structirlba_1_1Irlba_1_1Defaults.html#a5bcbb0f825ebb62a2626de2399cb6e6a',1,'irlba::Irlba::Defaults']]]
 ];
--- docs/search/functions_b.js
+++ docs/search/functions_b.js
@@ -3,12 +3,14 @@ var searchData=
   ['scaled_0',['Scaled',['../structirlba_1_1Scaled.html#a1b2c01cf3f066d871702fec3948cfaa3',1,'irlba::Scaled']]],
   ['set_5fconvergence_5ftolerance_1',['set_convergence_tolerance',['../classirlba_1_1Irlba.html#a9aa27d5a7891b6350b4d2da80b9b99d5',1,'irlba::Irlba']]],
   ['set_5fepsilon_2',['set_epsilon',['../classirlba_1_1LanczosBidiagonalization.html#af0ab346a06e95819dacb124d5827bebf',1,'irlba::LanczosBidiagonalization']]],
-  ['set_5finvariant_5ftolerance_3',['set_invariant_tolerance',['../classirlba_1_1Irlba.html#a4ce4790f38031dfc6ac715f1aa3666e2',1,'irlba::Irlba']]],
-  ['set_5fmaxit_4',['set_maxit',['../classirlba_1_1Irlba.html#ab2c04c1495a025b247553d85bea073ac',1,'irlba::Irlba']]],
-  ['set_5fnumber_5',['set_number',['../classirlba_1_1Irlba.html#a64dcc77838d579950f7eb05243852743',1,'irlba::Irlba']]],
-  ['set_5fseed_6',['set_seed',['../classirlba_1_1Irlba.html#a517d0cced89773e990aa625e433d7af5',1,'irlba::Irlba']]],
-  ['set_5fsingular_5fvalue_5fratio_5ftolerance_7',['set_singular_value_ratio_tolerance',['../classirlba_1_1Irlba.html#a1d25ffa06b6918f48afe37559958d000',1,'irlba::Irlba']]],
-  ['set_5fsvtol_8',['set_svtol',['../structirlba_1_1ConvergenceTest.html#a3c812f10291fc61aaf042c39531fe71e',1,'irlba::ConvergenceTest']]],
-  ['set_5ftol_9',['set_tol',['../structirlba_1_1ConvergenceTest.html#a453d8d3b30ea1143712d6e127c50af3c',1,'irlba::ConvergenceTest']]],
-  ['set_5fwork_10',['set_work',['../classirlba_1_1Irlba.html#a4ca6679e24ecfde03d952589d6654e0e',1,'irlba::Irlba']]]
+  ['set_5fexact_5ffor_5flarge_5fnumber_3',['set_exact_for_large_number',['../classirlba_1_1Irlba.html#aea4381224b805c16c3108f6685ae0c17',1,'irlba::Irlba']]],
+  ['set_5fexact_5ffor_5fsmall_5fmatrix_4',['set_exact_for_small_matrix',['../classirlba_1_1Irlba.html#ade92590314a777c7a476511516d4af5d',1,'irlba::Irlba']]],
+  ['set_5finvariant_5ftolerance_5',['set_invariant_tolerance',['../classirlba_1_1Irlba.html#a4ce4790f38031dfc6ac715f1aa3666e2',1,'irlba::Irlba']]],
+  ['set_5fmaxit_6',['set_maxit',['../classirlba_1_1Irlba.html#ab2c04c1495a025b247553d85bea073ac',1,'irlba::Irlba']]],
+  ['set_5fnumber_7',['set_number',['../classirlba_1_1Irlba.html#a64dcc77838d579950f7eb05243852743',1,'irlba::Irlba']]],
+  ['set_5fseed_8',['set_seed',['../classirlba_1_1Irlba.html#a517d0cced89773e990aa625e433d7af5',1,'irlba::Irlba']]],
+  ['set_5fsingular_5fvalue_5fratio_5ftolerance_9',['set_singular_value_ratio_tolerance',['../classirlba_1_1Irlba.html#a1d25ffa06b6918f48afe37559958d000',1,'irlba::Irlba']]],
+  ['set_5fsvtol_10',['set_svtol',['../structirlba_1_1ConvergenceTest.html#a3c812f10291fc61aaf042c39531fe71e',1,'irlba::ConvergenceTest']]],
+  ['set_5ftol_11',['set_tol',['../structirlba_1_1ConvergenceTest.html#a453d8d3b30ea1143712d6e127c50af3c',1,'irlba::ConvergenceTest']]],
+  ['set_5fwork_12',['set_work',['../classirlba_1_1Irlba.html#a4ca6679e24ecfde03d952589d6654e0e',1,'irlba::Irlba']]]
 ];
