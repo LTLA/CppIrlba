d113f3d8fab11433c1634a9e25a0331786a9e246
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-09-16 18:48:13 +0000

    Deploying to gh-pages from @ LTLA/CppIrlba@bace9baf758ad396c3d1b8d2d090eb92891aee45 🚀

--- docs/search/variables_0.js
+++ docs/search/variables_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['converged_0',['converged',['../structirlba_1_1Irlba_1_1Results.html#a6bf7cdbdef24df8f02d7e309ee7c7dcc',1,'irlba::Irlba::Results']]]
+  ['cap_5fnumber_0',['cap_number',['../structirlba_1_1Irlba_1_1Defaults.html#afc6a52eec01e7c1d0234d67f5e65428e',1,'irlba::Irlba::Defaults']]],
+  ['converged_1',['converged',['../structirlba_1_1Irlba_1_1Results.html#a6bf7cdbdef24df8f02d7e309ee7c7dcc',1,'irlba::Irlba::Results']]]
 ];
