fc47118f7323d6f5b7dcd8616f0c815bb766c10b
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-06-22 12:01:41 -0700

    Bring the R tests back online.

--- tests/R/test.cpp
+++ tests/R/test.cpp
@@ -7,16 +7,17 @@
 
 // [[Rcpp::export(rng=false)]]
 Rcpp::List run_irlba(Rcpp::NumericMatrix x, Rcpp::NumericVector init, int number, int work) {
-    irlba::Irlba irl;
-    irl.set_number(number).set_work(work);
+    irlba::Options opt;
+    opt.extra_work = work;
 
     Eigen::VectorXd v(init.size());
     std::copy(init.begin(), init.end(), v.data());
+    opt.initial = &v;
 
     // Can't be bothered making a Map, just copying everything for testing purposes.
     Eigen::MatrixXd A(x.nrow(), x.ncol());
     std::copy(x.begin(), x.end(), A.data());
-    auto res = irl.run(A, irlba::null_rng(), &v);
+    auto res = irlba::compute(A, number, opt);
 
     Rcpp::NumericMatrix outU(res.U.rows(), res.U.cols());
     std::copy(res.U.data(), res.U.data() + res.U.size(), outU.begin());
