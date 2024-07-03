86e6e998ebece07e8fdd7c13cb0b9bbf5ce8b72c
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-07-03 05:59:00 +0000

    Deploying to gh-pages from @ LTLA/CppIrlba@ab12fda22d938c1ed0393cd95fdc5d94490d2f8f 🚀

--- docs/search/all_a.js
+++ docs/search/all_a.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['parallel_2ehpp_0',['parallel.hpp',['../parallel_8hpp.html',1,'']]],
-  ['parallelsparsematrix_1',['parallelsparsematrix',['../classirlba_1_1ParallelSparseMatrix.html',1,'irlba::ParallelSparseMatrix&lt; ValueArray_, IndexArray_, PointerArray_ &gt;'],['../classirlba_1_1ParallelSparseMatrix.html#ad5a65986ddb8bf72081857b73d96d015',1,'irlba::ParallelSparseMatrix::ParallelSparseMatrix()'],['../classirlba_1_1ParallelSparseMatrix.html#a33b8ef94cfde4cff7c044593fc511453',1,'irlba::ParallelSparseMatrix::ParallelSparseMatrix(Eigen::Index nrow, Eigen::Index ncol, ValueArray_ x, IndexArray_ i, PointerArray_ p, bool column_major, int nthreads)']]],
-  ['pointertype_2',['PointerType',['../classirlba_1_1ParallelSparseMatrix.html#a8f65cf74a1199c98737b9a9431dc3137',1,'irlba::ParallelSparseMatrix']]]
+  ['parallelsparsematrix_1',['parallelsparsematrix',['../classirlba_1_1ParallelSparseMatrix.html',1,'irlba::ParallelSparseMatrix&lt; ValueArray_, IndexArray_, PointerArray_, EigenVector_ &gt;'],['../classirlba_1_1ParallelSparseMatrix.html#acf9a12ed3c0c2b532074d67b1514753f',1,'irlba::ParallelSparseMatrix::ParallelSparseMatrix()'],['../classirlba_1_1ParallelSparseMatrix.html#ada7b80f2024172e70c4677cf91423eb1',1,'irlba::ParallelSparseMatrix::ParallelSparseMatrix(Eigen::Index nrow, Eigen::Index ncol, ValueArray_ x, IndexArray_ i, PointerArray_ p, bool column_major, int nthreads)']]],
+  ['pointertype_2',['PointerType',['../classirlba_1_1ParallelSparseMatrix.html#a05d67b62dd461e6dc71e4f0d870b76f7',1,'irlba::ParallelSparseMatrix']]]
 ];
